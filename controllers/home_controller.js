28ae23a7ffc3e0296b96cb590b6e91d71ff16af3
sarthak khare <kharesarthak09@gmail.com>
2020-11-19 19:46:15 +0530

    created post module added routes for post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,19 @@
+const Post = require('../models/post');
 module.exports.home = function(req,res){
-    return res.end('<h1>yay working</h1>');
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "intsocial | Home",
+    //         posts:  posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "intsocial | Home",
+            posts:  posts
+        });
+    })
+
+
 }
