22c22b05afce0c104d11a429bfa719895552b899
sarthak khare <kharesarthak09@gmail.com>
2020-11-20 16:31:19 +0530

    add and delete comment on posts

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
-
+//const { post } = require('../routes');
+const Comment = require('../models/comment');
 module.exports.create = function(req,res){
     Post.create({
         content: req.body.content,
@@ -9,4 +10,22 @@ module.exports.create = function(req,res){
 
         return res.redirect('back');
     });
+}
+
+module.exports.destroy = function(req,res){
+    //to destroy we need id which we will get from params
+    Post.findById(req.params.id,function(err,post){
+
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post : req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+
+    })
 }
